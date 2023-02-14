04e9811b5bc5cefb3ff5db63b4238e689fa63d88
Coding-Cactus <codingcactus.cc@gmail.com>
2023-02-14 20:39:26 +0000

    :sparkles: Add error pages

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module ContactV3
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.eager_load_paths += %W(#{config.root}/lib/modules)
+    config.eager_load_paths += %W[#{config.root}/lib/modules]
+
+    config.exceptions_app = routes
   end
 end
