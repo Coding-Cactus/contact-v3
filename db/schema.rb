7f634a7c96570c16686e8cbe5641b429c52f1632
Coding-Cactus <codingcactus.cc@gmail.com>
2023-02-14 13:54:14 +0000

    :card_file_box: Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_171209) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_204331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
+  create_table "contact_comments", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "ticket_id", null: false
     t.text "content"
     t.boolean "moderator"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["ticket_id"], name: "index_comments_on_ticket_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+    t.index ["ticket_id"], name: "index_contact_comments_on_ticket_id"
+    t.index ["user_id"], name: "index_contact_comments_on_user_id"
   end
 
-  create_table "infractions", force: :cascade do |t|
+  create_table "contact_infractions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "action"
     t.string "reason"
@@ -34,48 +34,48 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_12_171209) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "ticket_id", null: false
-    t.index ["ticket_id"], name: "index_infractions_on_ticket_id"
-    t.index ["user_id"], name: "index_infractions_on_user_id"
+    t.index ["ticket_id"], name: "index_contact_infractions_on_ticket_id"
+    t.index ["user_id"], name: "index_contact_infractions_on_user_id"
   end
 
-  create_table "statuses", force: :cascade do |t|
+  create_table "contact_statuses", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_statuses_on_name"
+    t.index ["name"], name: "index_contact_statuses_on_name"
   end
 
-  create_table "tickets", force: :cascade do |t|
+  create_table "contact_tickets", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "type_id", null: false
     t.integer "status_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["status_id"], name: "index_tickets_on_status_id"
-    t.index ["type_id"], name: "index_tickets_on_type_id"
-    t.index ["user_id"], name: "index_tickets_on_user_id"
+    t.index ["status_id"], name: "index_contact_tickets_on_status_id"
+    t.index ["type_id"], name: "index_contact_tickets_on_type_id"
+    t.index ["user_id"], name: "index_contact_tickets_on_user_id"
   end
 
-  create_table "types", force: :cascade do |t|
+  create_table "contact_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_types_on_name"
+    t.index ["name"], name: "index_contact_types_on_name"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "contact_users", force: :cascade do |t|
     t.string "username"
     t.string "pfp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "tickets"
-  add_foreign_key "comments", "users"
-  add_foreign_key "infractions", "tickets"
-  add_foreign_key "infractions", "users"
-  add_foreign_key "tickets", "statuses"
-  add_foreign_key "tickets", "types"
-  add_foreign_key "tickets", "users"
+  add_foreign_key "contact_comments", "contact_tickets", column: "ticket_id"
+  add_foreign_key "contact_comments", "contact_users", column: "user_id"
+  add_foreign_key "contact_infractions", "contact_tickets", column: "ticket_id"
+  add_foreign_key "contact_infractions", "contact_users", column: "user_id"
+  add_foreign_key "contact_tickets", "contact_statuses", column: "status_id"
+  add_foreign_key "contact_tickets", "contact_types", column: "type_id"
+  add_foreign_key "contact_tickets", "contact_users", column: "user_id"
 end
